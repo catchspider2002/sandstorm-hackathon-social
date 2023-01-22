8007761d65655c61899ac85c02fe615d13a57e7a
Naveen CS <catchspider2002@gmail.com>
2023-01-22 15:03:45 +0530

    Added buffer

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 // import nodePolyfills from 'rollup-plugin-polyfill-node';
-// import inject from '@rollup/plugin-inject';
+import inject from '@rollup/plugin-inject';
 
 const config = {
 	plugins: [
 		sveltekit()
 		// , nodePolyfills()
-	]
+	],
 	// define: {
 	// global: {}
 	// process: {}
 	// 'process.env.ANCHOR_BROWSER': true
 	// }
-	// build: {
-	// 	rollupOptions: {
-	// 		plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })]
-	// 	}
-	// }
+	build: {
+		rollupOptions: {
+			plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })]
+		}
+	}
 };
 
 export default config;
